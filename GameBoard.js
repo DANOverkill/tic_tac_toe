c41ba2b167c2ecbc0211785b8f32c0e83489d63e
DANOverkill <danielz.nas@gmail.com>
2025-10-26 02:28:13 -0300

    feat: link main.js to HTML and set up GameBoard module
    - Added <script type='module'> for main.js in index.html
    - Imported GameBoard API into main.js
    - Added export default in GameBoard.js
    - Temporarily exposed GameBoard on window for console testing during development"

--- GameBoard.js
+++ GameBoard.js
@@ -20,5 +20,5 @@ const GameBoard = (() => {
 
 })();
 
-
+export default GameBoard;
 
---
+++ main.js
@@ -0,0 +1,7 @@
+// import the APIs
+import GameBoard from './GameBoard.js';
+// import Player from  './Player.js';
+// import GameControl from './GameControl.js';
+
+// expozing GameBoard temporatly during dev and testing
+window.GameBoard = GameBoard;
