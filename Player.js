6f5927481f47aa389578ac291b88b8556e88d988
Daniel <danielz.nas@gmail.com>
2025-10-26 22:32:28 -0300

    feat: add Player factory module
    - create players with name and mark
    - export default Player for importing in main.js
    - import Player in main.js
    - expose Player to window for development testing

---
+++ Player.js
@@ -0,0 +1,10 @@
+const Player = (name, mark) => {
+
+    const getName = () => name;
+    const getMark = () => mark;
+
+    return {getName, getMark};
+
+};
+
+export default Player;
--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 // import the APIs
 import GameBoard from './GameBoard.js';
-// import Player from  './Player.js';
+import Player from  './Player.js';
 // import GameControl from './GameControl.js';
 
 // expozing GameBoard temporatly during dev and testing
 window.GameBoard = GameBoard;
+window.Player = Player;
