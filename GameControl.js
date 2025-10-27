d27a71eb7354431b47cdcc69f1fd006b0ae47c58
Daniel <danielz.nas@gmail.com>
2025-10-27 00:37:06 -0300

    chore: added a console.log() in playturn method to help with dev while we still have no UI

--- GameControl.js
+++ GameControl.js
@@ -17,6 +17,7 @@ const GameControl = (() => {
     const playTurn = (index) => {
         GameBoard.setMark(index, currentPlayer.getMark());
         switchTurn();
+        console.log(GameBoard.getBoard()); //added console log for testing while we have no UI
     };
 
     return {playTurn};
