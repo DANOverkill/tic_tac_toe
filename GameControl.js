b86b72b9f909baa3d005d0bf95364dbdee4974c5
Daniel <danielz.nas@gmail.com>
2025-10-26 23:34:21 -0300

    feat: add GameControl module
    - import Player from Player.js and GameBoard from GameBoard.js
    - trigger prompts to get players' names
    - default to "Player 1" and "Player 2" if no name is provided
    - assign playerOne the "X" mark and playerTwo the "O" mark
    - playTurn receives an index parameter and uses GameBoard.setMark to apply the current player's mark

--- GameControl.js
+++ GameControl.js
@@ -1,3 +1,26 @@
-GameControl => {
-    
-}
+import GameBoard from "./GameBoard";
+import Player from "./Player";
+
+const GameControl = (() => {
+    const playerOneName = prompt('Please enter Name for Player 1:');
+    const playerTwoName = prompt('Please enter Name for Player 2:');
+
+    const playerOne = Player(playerOneName || 'Player 1', 'X');
+    const playerTwo = Player(playerTwoName || 'Player 2', 'X');
+
+    let currentPlayer = playerOne; 
+
+    const switchTurn = () => {
+        currentPlayer = currentPlayer === playerOne ? playerTwo : playerOne;
+    };
+
+    const playTurn = (index) => {
+        GameBoard.setMark(index, currentPlayer.getMark());
+        switchTurn();
+    };
+
+    return {playTurn};
+
+})();
+
+export default GameControl;
