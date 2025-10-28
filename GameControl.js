1c86969359ef3392526de06208c3516fec07cf97
DANOverkill <danielz.nas@gmail.com>
2025-10-27 23:54:23 -0300

    feat: add checkWin logic && add checkWin to the playTurn logic
    - checkWin logic filters through winPatters
    - for each win pattern, we filter through indexes
    - use .includes to check for indexes from getPlayerIndexes()
    - if checkwin returns true console.log(`${currentPlayer.getName(}) wins`)
    - else continues.

--- GameControl.js
+++ GameControl.js
@@ -14,18 +14,7 @@ const GameControl = (() => {
         currentPlayer = currentPlayer === playerOne ? playerTwo : playerOne;
     };
 
-    //win condition check
-    const winPatterns = [
-        [0, 1, 2], // Top row
-        [3, 4, 5], // Middle row
-        [6, 7, 8], // Bottom row
-        [0, 3, 6], // Left column
-        [1, 4, 7], // Middle column
-        [2, 5, 8], // Right column
-        [0, 4, 8], // Diagonal (top-left to bottom-right)
-        [2, 4, 6]  // Diagonal (top-right to bottom-left)
-    ];
-    
+    //check for players marked indexes on the board
     const getPlayerIndexes = (mark) => {
         const board = GameBoard.getBoard();
         const indexes = [];
@@ -38,12 +27,50 @@ const GameControl = (() => {
         }
         return indexes;
     };
+
+    //win condition check
+    const winPatterns = [
+        [0, 1, 2], // Top row
+        [3, 4, 5], // Middle row
+        [6, 7, 8], // Bottom row
+        [0, 3, 6], // Left column
+        [1, 4, 7], // Middle column
+        [2, 5, 8], // Right column
+        [0, 4, 8], // Diagonal (top-left to bottom-right)
+        [2, 4, 6]  // Diagonal (top-right to bottom-left)
+    ];
     
+    const checkWin = (playerIndex) => {
+        for (let i = 0; i < winPatterns.length; i++) {
+            const pattern = winPatterns[i];
+
+            let hasAllIndexes = true;
+
+            for (let j = 0; j < pattern.length; j++) {
+                const index = pattern[j];
+                if (!playerIndex.includes(index)) {
+                    hasAllIndexes = false;
+                    break;
+                }
+            }
+            if (hasAllIndexes === true) {
+                return true;
+            } 
+            
+        } 
+        return false;
+    };
+
     //round logic
     const playTurn = (index) => {
         GameBoard.setMark(index, currentPlayer.getMark());
-        switchTurn();
-        console.log(GameBoard.getBoard()); //added console log for testing while we have no UI
+
+        if (checkWin(getPlayerIndexes(currentPlayer.getMark()))) {
+            console.log(`${currentPlayer.getName()} wins!`)
+        } else {
+            switchTurn();
+            console.log(GameBoard.getBoard()); //added console log for testing while we have no UI
+        }
     };
 
     return {playTurn, getPlayerIndexes, currentPlayer};
