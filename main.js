8e22f09fe39fd528a842f362eca2f8fe76a036da
DANOverkill <danielz.nas@gmail.com>
2025-10-29 21:13:10 -0300

    Feat: create logic for dynamically generate  board UI
    - cache board DOM in main.js
    - createBoardUI logic for generating divs in index.html
    - each div gets a dataset with index from 0 to 9
    - startBtn now calls createBoardUI
    - restartBtn runs board.innerHTML = ''; to clear board UI
    - add basic style to board fo dev purposes

--- main.js
+++ main.js
@@ -15,20 +15,32 @@ window.startGame = (playerOneName, playerTwoName) => {
    return game;
 };
 
-
+// DOM cache
 const startBtn = document.querySelector('#startGameBtn');
 const player1Input = document.querySelector('#player1Name');
 const player2Input = document.querySelector('#player2Name');
 const setupScreen = document.querySelector('#setupScreen');
 const gameScreen = document.querySelector('#gameScreen');
 const restartBtn = document.querySelector('#restartBtn');
+const board = document.querySelector('#board');
 // Ai not current implemented
 // const player1IsAI = document.querySelector('#player1IsAI');
 // const player2IsAI = document.querySelector('#player2IsAI');
 
+function createBoardUI() {
+  board.innerHTML = '';
+
+  for (let i = 0; i < 9; i++) {
+  const cell = document.createElement('div');
+  cell.classList.add('cell');
+  cell.dataset.index = i;
+  board.appendChild(cell);
+  }
+};
+
 function validateInputs() {
   startBtn.disabled = !(player1Input.value && player2Input.value);
-}
+};
 
 player1Input.addEventListener('input', validateInputs);
 player2Input.addEventListener('input', validateInputs);
@@ -36,6 +48,7 @@ player2Input.addEventListener('input', validateInputs);
 startBtn.addEventListener('click', () => {
   setupScreen.classList.add('hidden');
   gameScreen.classList.remove('hidden');
+  createBoardUI();
   startGame(player1Input.value, player2Input.value);
 });
 
@@ -46,6 +59,7 @@ restartBtn.addEventListener('click', () => {
    player1Input.placeholder = 'Player 1 name';
    player2Input.placeholder = 'Player 2 name';
    startBtn.disabled = true;
+   board.innerHTML = '';
    // document.querySelector('#player1AI').checked = false;
    // document.querySelector('#player2AI').checked = false;
 });
