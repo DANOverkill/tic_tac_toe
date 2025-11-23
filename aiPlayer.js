7a68072acfc33737661cac508e47daf7a94a6ec9
Daniel <danielz.nas@gmail.com>
2025-11-23 15:27:55 -0400

    feat: created whichMark() in aiPlayer that checks for what mark ai will use - if player one is checked for ai, then ai player will be created with 'X' mark - if player two is checked for ai, then ai player will be created with 'O' mark - none of the code for aiPlayer.js has been tested yet - checkmarks in index.html is still commented out

--- aiPlayer.js
+++ aiPlayer.js
@@ -14,7 +14,15 @@ const aiPlayer = () => {
 
     let aiPlayerMark; 
 
-    let whichMark = () => {
-        // logic for checking 
+    let whichMark = (player1, player2) => {
+        let player1 = player1Input.checked
+        let played2 = player2Input.checked
+        if (player1 === true) {
+            console.log('player one is ai, and will have an X for mark')
+            return aiPlayerMark = 'X';
+        } else if (played2 === true) {
+            console.log('player two is ai, and will have an O for mark')
+            return aiPlayerMark = 'O';
+        }
     }; 
 }
